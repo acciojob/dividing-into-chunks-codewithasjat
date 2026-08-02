1c7a30e58a7057407eaf4805ac882539183a3919
codewithasjat <128043755+codewithasjat@users.noreply.github.com>
2026-08-02 20:11:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  // Write your code here
+    let result = [];
+    let current = [];
+    let sum = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+
+        if (sum + arr[i] <= n) {
+            current.push(arr[i]);
+            sum += arr[i];
+        } 
+        else {
+            result.push(current);
+            current = [arr[i]];
+            sum = arr[i];
+        }
+    }
+
+    if (current.length > 0) {
+        result.push(current);
+    }
+
+    return result;
 };
 
-const n = prompt("Enter n: ");
+const n = Number(prompt("Enter n: "));
 alert(JSON.stringify(divide(arr, n)));
